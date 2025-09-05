2206bf4bcbc7ec2a9e8aa1d18f4997fe8f45ba2f
Vinicius Souza <vinicipkt@live.com>
2025-09-05 18:41:05 -0300

    deploy issue

--- app/layout.js
+++ app/layout.js
@@ -22,20 +22,22 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        <header>
-          <nav className="navbar bg-base-300 justify-between">
-            <div>
-              <Filters />
-            </div>
-            <Search />
-            <div className='flex gap-2'>
-              <AddMovieBtn />
-              <HomeBtn />
-              <ShowMsgContainer />
-            </div>
-          </nav>
-        </header>
-        <Suspense fallback={<div>Loading...</div>}>{children} </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <header>
+            <nav className="navbar bg-base-300 justify-between">
+              <div>
+                <Filters />
+              </div>
+              <Search />
+              <div className='flex gap-2'>
+                <AddMovieBtn />
+                <HomeBtn />
+                <ShowMsgContainer />
+              </div>
+            </nav>
+          </header>
+          {children}
+        </Suspense>
       </body>
     </html >
   );
